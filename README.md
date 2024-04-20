41fd20e46fa335be065afb928abecac6b90faffc
Kebii <zjiaxu@whu.edu.cn>
2024-04-20 10:39:00 +0800

    new file:   README.md

---
+++ README.md
@@ -0,0 +1,93 @@
+# Freehand-Gensin-Diffusion
+Transferring Gensin PV into freehand style with Diffusion Model.
+
+# Plans
+- [x] Inference codes of image-model
+- [x] Pretrained weights for 480x320 resolution
+- [ ] Pretrained weights for 1280x720 resolution
+- [ ] Inference codes of video-model incorporating temporal module.
+- [ ] Training scripts
+
+
+# Examples 
+
+Here are some results we generated, with the resolution of 480x320.
+
+https://github.com/Kebii/Freehand-Gensin-Diffusion
+
+https://github.com/Kebii/Freehand-Gensin-Diffusion
+
+# Limitations
+We observe following shortcomings in current version:
+1. The primary issue is the temporal inconsistency in the generated frames, which causes flickering and jittering in the video.
+2. Training and inference for this model are inefficient, requiring substantial computational resources.
+
+# Installation
+
+## Build Environtment
+
+We Recommend a python version `>=3.10` and cuda version `=11.7`. Then build environment as follows:
+
+```shell
+conda create -n Gensin python=3.10
+conda activate Gensin
+# Install requirements with pip:
+pip install -r requirements.txt
+```
+
+## Download weights
+
+You can download weights manually, which has some steps:
+
+1. Download our trained weights from BaiduDisk(https://), which include two parts: `denoising_unet.pth`, `reference_unet.pth`.
+
+2. Download pretrained weight of based models and other components: 
+    - [StableDiffusion V1.5](https://huggingface.co/runwayml/stable-diffusion-v1-5)
+    - [sd-vae-ft-mse](https://huggingface.co/stabilityai/sd-vae-ft-mse)
+    - [image_encoder](https://huggingface.co/lambdalabs/sd-image-variations-diffusers/tree/main/image_encoder)
+
+3. Download dwpose weights (`dw-ll_ucoco_384.onnx`, `yolox_l.onnx`) following [this](https://github.com/IDEA-Research/DWPose?tab=readme-ov-file#-dwpose-for-controlnet).
+
+Finally, these weights should be orgnized as follows:
+
+```text
+./pretrained_weights/
+|-- denoising_unet.pth
+|-- reference_unet.pth
+|-- DWPose
+|   |-- dw-ll_ucoco_384.onnx
+|   `-- yolox_l.onnx
+|-- image_encoder
+|   |-- config.json
+|   `-- pytorch_model.bin
+|-- sd-vae-ft-mse
+|   |-- config.json
+|   |-- diffusion_pytorch_model.bin
+|   `-- diffusion_pytorch_model.safetensors
+`-- stable-diffusion-v1-5
+    |-- feature_extractor
+    |   `-- preprocessor_config.json
+    |-- model_index.json
+    |-- unet
+    |   |-- config.json
+    |   `-- diffusion_pytorch_model.bin
+    `-- v1-inference.yaml
+```
+
+# Inference 
+
+Here is the cli command for running inference scripts:
+
+```shell
+python -m scripts.paint_gensin --config ./configs/prompts/paint_gensin.yaml -W 480 -H 320
+```
+
+You can refer the format of `paint_gensin.yaml` and modify the `input_video_path` to transfer other Gensin PVs in MP4 format.
+
+# Disclaimer
+
+This project is intended for academic research, and we explicitly disclaim any responsibility for user-generated content. Users are solely liable for their actions while using the generative model. The project contributors have no legal affiliation with, nor accountability for, users' behaviors. It is imperative to use the generative model responsibly, adhering to both ethical and legal standards.
+
+# 🙏🏻 Acknowledgements
+
+This repository is build based on [Moore-AnimateAnyone](https://github.com/MooreThreads/Moore-AnimateAnyone). We thank them for their excellent work in releasing high-quality code.
---
+++ src/dataset/gensin_image.py
@@ -0,0 +1,150 @@
+import json
+import random
+import os
+
+import torch
+import torchvision.transforms as transforms
+from decord import VideoReader
+from PIL import Image
+from torch.utils.data import Dataset
+from transformers import CLIPImageProcessor
+
+
+class GensinVdImgDataset(Dataset):
+    def __init__(
+        self,
+        img_size,
+        img_scale=(1.0, 1.0),
+        img_ratio=(0.9, 1.0),
+        drop_ratio=0.1,
+        srcdata_path="/data2/zjx/dataset/gensin_pv/raw",
+        tgtdata_path="/data2/zjx/dataset/gensin_pv/draw"
+    ):
+        super().__init__()
+
+        self.img_size = img_size
+        self.img_scale = img_scale
+        self.img_ratio = img_ratio
+
+        self.srcdata_path = srcdata_path
+        self.tgtdata_path = tgtdata_path
+        self.tgt_videofile_lst = os.listdir(tgtdata_path)
+        # self.tgt_videoname_lst = [vf[:-4] for vf in self.tgt_videofile_lst]        # remove the .mp4 suffix
+        # self.src_videofile_lst = [vf+"_ori.mp4" for vf in self.tgt_videoname_lst]
+
+        self.transform = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(
+                    self.img_size,
+                    scale=self.img_scale,
+                    ratio=self.img_ratio,
+                    interpolation=transforms.InterpolationMode.BILINEAR,
+                ),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),
+            ]
+        )
+
+        self.drop_ratio = drop_ratio
+
+    def augmentation(self, image, transform, state=None):
+        if state is not None:
+            torch.set_rng_state(state)
+        return transform(image)
+
+    def __getitem__(self, index):
+        tgt_videofile = self.tgt_videofile_lst[index]
+        src_videofile = tgt_videofile[:-4] + "_ori.mp4"
+
+        tgt_video_reader = VideoReader(os.path.join(self.tgtdata_path, tgt_videofile))
+        src_video_reader = VideoReader(os.path.join(self.srcdata_path, src_videofile))
+
+        assert len(tgt_video_reader) == len(
+            src_video_reader
+        ), f"{len(tgt_video_reader) = } != {len(src_video_reader) = } in {tgt_videofile}"
+
+        video_length = len(tgt_video_reader)
+
+        img_idx = random.randint(0, video_length - 1)
+        tgt_img = tgt_video_reader[img_idx]
+        tgt_img_pil = Image.fromarray(tgt_img.asnumpy())
+        src_img = src_video_reader[img_idx]
+        src_img_pil = Image.fromarray(src_img.asnumpy())
+
+        state = torch.get_rng_state()
+        tgt_img = self.augmentation(tgt_img_pil, self.transform, state)
+        src_img = self.augmentation(src_img_pil, self.transform, state)
+        
+        sample = dict(
+            tgt_videofile=tgt_videofile,
+            tgt_img=tgt_img,
+            src_img=src_img,
+        )
+
+        return sample
+
+class GensinImgDataset(Dataset):
+    def __init__(
+        self,
+        img_size,
+        img_scale=(1.0, 1.0),
+        img_ratio=(0.9, 1.0),
+        drop_ratio=0.1,
+        srcdata_path="/data2/zjx/dataset/gensin_pv/raw_img",
+        tgtdata_path="/data2/zjx/dataset/gensin_pv/draw_img"
+    ):
+        super().__init__()
+
+        self.img_size = img_size
+        self.img_scale = img_scale
+        self.img_ratio = img_ratio
+
+        self.srcdata_path = srcdata_path
+        self.tgtdata_path = tgtdata_path
+        self.img_file_lst = os.listdir(srcdata_path)
+
+        self.clip_image_processor = CLIPImageProcessor()
+
+        self.transform = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(
+                    self.img_size,
+                    scale=self.img_scale,
+                    ratio=self.img_ratio,
+                    interpolation=transforms.InterpolationMode.BILINEAR,
+                ),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]),
+            ]
+        )
+
+        self.drop_ratio = drop_ratio
+
+    def augmentation(self, image, transform, state=None):
+        if state is not None:
+            torch.set_rng_state(state)
+        return transform(image)
+
+    def __getitem__(self, index):
+        file_name = self.img_file_lst[index]
+        tgt_img_pil = Image.open(os.path.join(self.tgtdata_path, file_name))
+        src_img_pil = Image.open(os.path.join(self.srcdata_path, file_name))
+
+        state = torch.get_rng_state()
+        tgt_img = self.augmentation(tgt_img_pil, self.transform, state)
+        src_img = self.augmentation(src_img_pil, self.transform, state)
+        clip_image = self.clip_image_processor(
+            images=src_img_pil, return_tensors="pt"
+        ).pixel_values[0]
+        
+        sample = dict(
+            file_name=file_name,
+            tgt_img=tgt_img,
+            src_img=src_img,
+            clip_img=clip_image
+        )
+
+        return sample
+
+    def __len__(self):
+        return len(self.img_file_lst)
